1cdace4d76b8edb8d599d899805806dd105b3fda
samuelsonric <samuelson.rick@gmail.com>
2023-01-29 11:49:07 -0800

    Added testset `Structure`; cleaned up testset `KalmanFilter`.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,40 @@
 using AlgebraicInference
 using Catlab, Catlab.Theories
+using LinearAlgebra: norm
 using Test
 
+import Base: ≈
+
+const FC = FreeAbelianBicategoryRelations
+
+function ≈(F₁::QuadraticBifunction, F₂::QuadraticBifunction)
+    Q₁, a₁, α₁, B₁, b₁ = conjugate(F₁)
+    Q₂, a₂, α₂, B₂, b₂ = conjugate(F₂)
+
+    atol = 1e-3
+    return (
+        Q₁ ≈ Q₂
+        && isapprox(a₁, a₂; atol)
+        && isapprox(α₁, α₂; atol)
+        && isapprox(B₁, B₂; atol)
+        && isapprox(b₁, zero(b₁); atol) == isapprox(b₂, zero(b₂); atol)
+    )
+end
+
 @testset "KalmanFilter" begin
-    O = mzero(FreeAbelianBicategoryRelations.Ob)
-    X = Ob(FreeAbelianBicategoryRelations, :X)
-    Q = Hom(:Q, O, X)
+    X = Ob(FC, :X)
     F = Hom(:F, X, X)
+    Q = Hom(:Q, mzero(FC.Ob), X)
     kalman_predict = (F ⊕ Q) ⋅ plus(X)
    
-
-    Z = Ob(FreeAbelianBicategoryRelations, :Z)
-    R = Hom(:R, O, Z)
+    Z = Ob(FC, :Z)
     H = Hom(:H, X, Z)
+    R = Hom(:R, mzero(FC.Ob), Z)
     kalman_observe = Δ(X) ⋅ (id(X) ⊕ H ⊕ R) ⋅ (id(X) ⊕ plus(Z))
 
-    P₀ = Hom(:P₀, O, X)
-    z₁ = Hom(:z_1, O, Z)
-    z₂ = Hom(:z_2, O, Z)
-    
+    P₀ = Hom(:P_0, mzero(FC.Ob), X)
+    z₁ = Hom(:z_1, mzero(FC.Ob), Z)
+    z₂ = Hom(:z_2, mzero(FC.Ob), Z)
     kalman_filter = P₀
     kalman_filter = kalman_filter ⋅ kalman_predict ⋅ kalman_observe ⋅ (id(X) ⊕ dagger(z₁))
     kalman_filter = kalman_filter ⋅ kalman_predict ⋅ kalman_observe ⋅ (id(X) ⊕ dagger(z₂))
@@ -64,7 +80,7 @@ using Test
         z₂ => GaussianRelation(QuadraticFunction([-375.93, 301.78])),
     )
 
-    P₂, x₂, _... = params(functor(types, kalman_filter; generators))
+    d = functor(types, kalman_filter; generators)
     rtol = 1e-3
     
     @test isapprox([
@@ -74,8 +90,45 @@ using Test
         0       0       0       8.92    11.33   5.13
         0       0       0       11.33   61.1    75.4
         0       0       0       5.13    75.4    126.5
-    ], P₂; rtol)
+    ], cov(d); rtol)
 
-    @test isapprox([-378.9, 53.8, 94.5, 303.9, -22.3, -63.6], x₂; rtol)
+    @test isapprox([-378.9, 53.8, 94.5, 303.9, -22.3, -63.6], mean(d); rtol)
 end
 
+@testset "Structure" begin
+    A = QuadDom(1)
+    B = QuadDom(2)
+
+    #################################################################
+
+    @test (plus(A) ⊕ id(A)) ⋅ plus(A) ≈ (id(A) ⊕ plus(A)) ⋅ plus(A) # ⚪-as
+    @test plus(A) ≈ swap(A, A) ⋅ plus(A)                            # ⚪-co
+    @test (zero(A) ⊕ id(A)) ⋅ plus(A) ≈ id(A)                       # ⚪-unl
+    @test Δ(A) ⋅ (Δ(A) ⊕ id(A)) ≈ Δ(A) ⋅ (id(A) ⊕ Δ(A))             # ⚫-coas
+    @test Δ(A) ≈ Δ(A) ⋅ swap(A, A)                                  # ⚫-coco
+    @test Δ(A) ⋅ (◊(A) ⊕ id(A)) ≈ id(A)                             # ⚫-counl
+    @test (∇(A) ⊕ id(A)) ⋅ ∇(A) ≈ (id(A) ⊕ ∇(A)) ⋅ ∇(A)             # ⚫-as
+    @test ∇(A) ≈ swap(A, A) ⋅ ∇(A)                                  # ⚫-co 
+    @test (□(A) ⊕ id(A)) ⋅ ∇(A) ≈ id(A)                             # ⚫-unl
+
+    #################################################################
+
+    @test plus(A) ⋅ Δ(A) ≈ (Δ(A) ⊕ Δ(A)) ⋅ (id(A) ⊕ swap(A, A) ⊕ id(A)) ⋅ (plus(A) ⊕ plus(A))   # ⚪⚫-bi
+    @test zero(A) ⋅ Δ(A) ≈ zero(A) ⊕ zero(A)                                                    # ⚪⚫-biun
+    @test plus(A) ⋅ ◊(A) ≈ ◊(A) ⊕ ◊(A)                                                          # ⚫⚪-biun
+    @test zero(A) ⋅ ◊(A) ≈ id(mzero(QuadDom))                                                   # ⚪⚫-bo
+
+    #################################################################
+
+    @test (id(A) ⊕ Δ(A)) ⋅ (∇(A) ⊕ id(A)) ≈ ∇(A) ⋅ Δ(A) # ⚫-fr1
+    @test (Δ(A) ⊕ id(A)) ⋅ (id(A) ⊕ ∇(A)) ≈ ∇(A) ⋅ Δ(A) # ⚫-fr2
+    @test Δ(A) ⋅ ∇(A) ≈ id(A)                           # ⚫-sp
+    @test □(A) ⋅ ◊(A) ≈ id(mzero(QuadDom))              # ⚫-bo
+
+    #################################################################
+
+    @test (id(A) ⊕ coplus(A)) ⋅ (plus(A) ⊕ id(A)) ≈ plus(A) ⋅ coplus(A) # ⚪-fr1
+    @test (coplus(A) ⊕ id(A)) ⋅ (id(A) ⊕ plus(A)) ≈ plus(A) ⋅ coplus(A) # ⚪-fr2
+    @test coplus(A) ⋅ plus(A) ≈ id(A)                                   # ⚪-sp
+    @test zero(A) ⋅ cozero(A) ≈ id(mzero(QuadDom))                      # ⚪-bo
+end
